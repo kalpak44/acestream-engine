a8cb2af5649438580f90db7926ce72256ae1d489
Pavel Ushanli <pau@foryouandyourcustomers.com>
2026-02-11 09:52:05 +0200

    Remove obsolete `test.sh` script

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -s "http://localhost:6878/server/api?token="$(
-        curl -s http://localhost:6878/server/api?method=get_api_access_token |
-        grep -oE '[[:xdigit:]]{64}'
-    )"&method=search" |
-grep -oE '"infohash": "[[:xdigit:]]{40}"'
-
